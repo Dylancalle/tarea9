3dcdd1d2f8776fb9d6d9594d4dc1b02519b97c19
Dylan Calle Contreras <dylan.calle@ucb.edu.bo>
2024-03-18 16:46:34 -0400

    Se declaro que la clase juego empieze con 0 en sus rolls

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,11 +1,14 @@
-
 class Game {
     constructor() {
       this.score = 0;
+      this.rolls = new Array(20).fill(0);
     }
     getscore() {
       return this.score;
     }
+    getrolls() {
+        return this.rolls;
+      }
   }
   
   module.exports = Game;
--- src/BowlingGameTest.spec.js
+++ src/BowlingGameTest.spec.js
@@ -1,10 +1,12 @@
 const Game = require('./BowlingGame.js'); 
-
+const game = new Game();
 describe("BowlingGame", () => {
     it("Deberia el score iniciar en 0", () => {
-        const game = new Game();
       expect(game.getscore()).toEqual(0);
     });
+    it("Deberia todos los espacios de rolls empezar en 0", () => {
+        for (let i = 0; i < 20; i++) {
+            expect(game.getrolls()[i]).toEqual(0);
+        }
+      });
   });
-  
-
