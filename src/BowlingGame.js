aaaf7f240cb6ad40a02de5a36badaeb2200e8c38
Dylan Calle Contreras <dylan.calle@ucb.edu.bo>
2024-03-19 01:15:07 -0400

    En el caso de que dos rolls sumen 10, sin que el segundo sea 0, se sumaran mas el roll siguiente a los dos

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,26 +1,31 @@
 class Game {
-    constructor() {
-      this.score = 0;
-      this.rolls = new Array(20).fill(0);
-    }
-    getscore() {
-      return this.score;
-    }
-    getrolls() {
-        return this.rolls;
-      }
-    llenar(){
-        for (let i = 0; i < 20; i++) {
-            this.rolls[i]=i;
-        }
-    }
-    calcularscore(){
-        for (let i = 0; i < 20; i++) {
-            this.score=this.score+this.rolls[i];
-        }
-    }
+  constructor() {
+    this.score = 0;
+    this.rolls = new Array(20).fill(0);
   }
-  
-  module.exports = Game;
+  getscore() {
+    return this.score;
+  }
+  getrolls() {
+      return this.rolls;
+    }
+  llenar(){
+      for (let i = 0; i < 20; i++) {
+          this.rolls[i]=i;
+      }
+  }
+  calcularscore(){
+      for (let i = 0; i < 20; i=i+2) {
+          this.score=this.score+this.rolls[i];
+          this.score=this.score+this.rolls[i+1];
+          if(this.rolls[i]+this.rolls[i+1]==10 && this.rolls[i]!=10)
+          {
+            this.score+=this.rolls[i+2];
+          }
+      }
+      
+  }
+}
 
-  
+module.exports = Game;
+
--- src/BowlingGameTest.spec.js
+++ src/BowlingGameTest.spec.js
@@ -1,15 +1,18 @@
-const Game = require('./BowlingGame.js'); 
-const game = new Game();
+import Game from './BowlingGame';
+
 describe("BowlingGame", () => {
     it("Deberia el score iniciar en 0", () => {
+        const game = new Game();
       expect(game.getscore()).toEqual(0);
     });
     it("Deberia todos los espacios de rolls empezar en 0", () => {
+        const game = new Game();
         for (let i = 0; i < 20; i++) {
             expect(game.getrolls()[i]).toEqual(0);
         }
     });
     it("deberia la suma de todos los rolls es el score", () => {
+        const game = new Game();
         let suma=0; 
         game.llenar();
         game.calcularscore();
@@ -18,4 +21,13 @@ describe("BowlingGame", () => {
         }
         expect(game.getscore()).toEqual(suma);
     });
+    it("deberia cuando un frame suma 10, sin que el segundo sea 0, que el frame tome un roll del siguiente frame", () => { 
+        const game = new Game();
+        game.rolls[0]=1;
+        game.rolls[1]=9;
+        game.rolls[2]=5;
+        game.calcularscore();
+        expect(game.getscore()).toEqual(20);
+    });
 });
+    
